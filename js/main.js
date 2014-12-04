90315d7821ec24451d1132dd6576eaeeaaeabc40
matsu-chara <matsuy00@gmail.com>
2014-12-04 20:33:43 +0900

    qlockの骨組みを作成

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+//QLOCK-写経 2014/12/02
 
+// namespace
+var qlock = qlock || {};
+
+(function() {
+  "use strict";
+  var $window = $(window),
+      $container = $(".container");
+
+  qlock.ContainerView($window, $container);
+} ());
